bd7890ce4a254f5ad339dd172900a67a521a8fa3
congbacoor <cong@bacoor.co>
2026-02-03 13:26:26 +0700

    fix ui

---
+++ app/manifest.ts
@@ -0,0 +1,45 @@
+import { MetadataRoute } from 'next'
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'Thay Hồng Toán',
+    short_name: 'Thay Hồng Toán',
+    description: 'Thay Hồng Toán',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#fff',
+    theme_color: '#fff',
+    icons: [
+      {
+        src: '36x36.png',
+        sizes: '36x36',
+        type: 'image/png',
+      },
+      {
+        src: '48x48.png',
+        sizes: '48x48',
+        type: 'image/png',
+      },
+      {
+        src: '72x72.png',
+        sizes: '72x72',
+        type: 'image/png',
+      },
+      {
+        src: '96x96.png',
+        sizes: '96x96',
+        type: 'image/png',
+      },
+      {
+        src: '144x144.png',
+        sizes: '144x144',
+        type: 'image/png',
+      },
+      {
+        src: '192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+    ],
+  }
+}
